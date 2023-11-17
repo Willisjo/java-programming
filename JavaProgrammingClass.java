95bef21d92b44d90cee3bf670e2442d48435f9ec
Willisjo <69597275+Willisjo@users.noreply.github.com>
2023-11-17 23:36:14 +1100

    Add files via upload

---
+++ JavaProgrammingClass.java
@@ -0,0 +1,5 @@
+public class JavaProgrammingClass {
+    public static void main(String[] args) {
+        System.out.println("This is java programming class.");
+        System.out.println("We will work in groups of 4s");
+    }
